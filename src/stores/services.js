c6f92c22e4d5bd121fd80409d8670e5f42478c5c
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2024-12-22 10:53:12 +0100

    change fetchservice

--- src/stores/services.js
+++ src/stores/services.js
@@ -1,26 +1,50 @@
-import { ref, onMounted } from "vue";
-import { defineStore } from "pinia";
-import ServicesAPI from "@/api/ServicesAPI";
+import { ref, onMounted } from 'vue'
+import { defineStore } from 'pinia'
+import ServicesAPI from '../api/ServicesAPI'
 
 export const useServicesStore = defineStore('services', () => {
-  const services = ref([]);
 
-  // Método para cargar los servicios manualmente
-  const fetchServices = async () => {
-    if (services.value.length > 0) return; // Evita recargar si ya están cargados
-    try {
-      const { data } = await ServicesAPI.all();
-      services.value = data;
-    } catch (error) {
-      console.error('Error al obtener los servicios:', error);
+    const services = ref([])
+
+    onMounted(async () => {
+        try {
+            const { data } = await ServicesAPI.all()
+            services.value = data
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    return {
+        services
     }
-  };
+})
 
-  // Carga automática al usar el store
-  onMounted(fetchServices);
 
-  return {
-    services,
-    fetchServices, // Exponemos el método para cargas explícitas
-  };
-});
+
+// import { ref, onMounted } from "vue";
+// import { defineStore } from "pinia";
+// import ServicesAPI from "@/api/ServicesAPI";
+
+// export const useServicesStore = defineStore('services', () => {
+//   const services = ref([]);
+
+//   // Método para cargar los servicios manualmente
+//   const fetchServices = async () => {
+//     if (services.value.length > 0) return; // Evita recargar si ya están cargados
+//     try {
+//       const { data } = await ServicesAPI.all();
+//       services.value = data;
+//     } catch (error) {
+//       console.error('Error al obtener los servicios:', error);
+//     }
+//   };
+
+//   // Carga automática al usar el store
+//   onMounted(fetchServices);
+
+//   return {
+//     services,
+//     fetchServices, // Exponemos el método para cargas explícitas
+//   };
+// });
